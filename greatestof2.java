a1b106befc5129ae76a54269656163d1fc8868a0
hackermani143 <120126686+hackermani143@users.noreply.github.com>
2022-12-20 16:32:19 +0530

    greatest of two numbers in java

---
+++ greatestof2.java
@@ -0,0 +1,19 @@
+
+
+public class greatestof2 {
+    public static void main(String[] args){
+        int a=10;
+        int b=1;
+        if(a>b)
+        {
+            System.out.println(a+" is greatest");
+        }else if(a==b)
+        {
+            System.out.println(" equal");
+        }
+        else{
+            System.out.println(b+" is biggest");
+        }
+    
+    }
+}
